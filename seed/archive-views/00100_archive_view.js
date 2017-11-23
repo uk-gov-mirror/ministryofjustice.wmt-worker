72548b1b304862bed8e8252940b3cdacada20d36
David McKinney <d.mckinney@kainos.com>
2017-11-23 17:05:16 +0000

    Remove unnecessary escape characters on concatenation of offender manager name

--- seed/archive-views/00100_archive_view.js
+++ seed/archive-views/00100_archive_view.js
@@ -6,7 +6,7 @@ exports.seed = function (knex, Promise) {
       om.UniqueIdentifier AS unique_identifier
     , ouLdu.Name AS ldu_name
     , ouTeam.Name AS team_name
-    , CONCAT(om.Forename, \' \', om.Surname) AS om_name
+    , CONCAT(om.Forename, ' ', om.Surname) AS om_name
     , w.TotalCases AS total_cases
     , w.TotalPoints AS total_points
     , w.SDRPoints AS sdr_points
@@ -23,7 +23,7 @@ exports.seed = function (knex, Promise) {
       JOIN dbo.Workload w ON om.Id = w.OffenderManagerId
       JOIN dbo.OrganisationalUnit ouTeam ON w.TeamId = ouTeam.Id 
       JOIN dbo.OrganisationalUnit ouLdu ON w.LduId = ouLdu.Id
-      GROUP BY om.UniqueIdentifier, ouLdu.Name, ouTeam.Name, CONCAT(om.Forename, \' \', om.Surname), w.TotalCases, 
+      GROUP BY om.UniqueIdentifier, ouLdu.Name, ouTeam.Name, CONCAT(om.Forename, ' ', om.Surname), w.TotalCases, 
       w.TotalPoints, w.SDRPoints, w.SDRConversionPoints, w.PAROMSPoints, w.NominalTarget, 
       w.ContractedHoursPerWeek, w.hoursReduction, n.HoursReduced, n.Notes, om.OffenderManagerTypeId;`
 
@@ -31,7 +31,7 @@ exports.seed = function (knex, Promise) {
       om.UniqueIdentifier AS unique_identifier
       , ouLdu.Name AS ldu_name
       , ouTeam.Name AS team_name
-      , CONCAT(om.Forename, \' \', om.Surname) AS om_name
+      , CONCAT(om.Forename, ' ', om.Surname) AS om_name
       , w.TotalCases AS total_cases
       , w.TotalPoints AS total_points
       , w.NominalTarget AS nominal_target
@@ -49,7 +49,7 @@ exports.seed = function (knex, Promise) {
       om.UniqueIdentifier AS unique_identifier
       , ouLdu.Name AS ldu_name
       , ouTeam.Name AS team_name
-      , CONCAT(om.Forename, \' \', om.Surname) AS om_name
+      , CONCAT(om.Forename, ' ', om.Surname) AS om_name
       , w.TotalCases AS total_cases
       , w.TotalPoints AS total_points
       , w.NominalTarget AS nominal_target
@@ -63,12 +63,12 @@ exports.seed = function (knex, Promise) {
       JOIN dbo.OrganisationalUnit ouLdu ON w.LduId = ouLdu.Id
       WHERE om.Id NOT IN (SELECT OffenderManagerId FROM dbo.Note)`
 
-      // cannot index with outer join 
-      //var index = 'CREATE UNIQUE CLUSTERED INDEX idx_archive_view on dbo.archive_view (unique_identifier)'
+      // cannot index with outer join
+      // var index = 'CREATE UNIQUE CLUSTERED INDEX idx_archive_view on dbo.archive_view (unique_identifier)'
 
   return knex.schema
     .raw('DROP VIEW IF EXISTS dbo.archive_view;')
     .raw('SET ARITHABORT ON')
     .raw(view)
-    //.raw(index)
+    // .raw(index)
 }
