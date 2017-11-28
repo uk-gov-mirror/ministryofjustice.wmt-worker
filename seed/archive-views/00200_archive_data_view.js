5ac1ff997cdaaa44b3c74f3d5d56574ca84ca08d
David McKinney <d.mckinney@kainos.com>
2017-11-28 15:06:49 +0000

    Archive data view working with indexed offender managers view

--- seed/archive-views/00200_archive_data_view.js
+++ seed/archive-views/00200_archive_data_view.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
     , om.om_type_id AS om_type_id
     , ouLdu.Name AS ldu_name
     , om.team_name AS team_name
-    , CONCAT(om.forename + ' ' + om.surname) AS om_name
+    , CONCAT(om.om_forename, ' ', om.om_surname) AS om_name
     , SUM(om.total_cases) AS total_cases
     , SUM(om.total_points) AS total_points
     , SUM(om.sdr_points) AS sdr_points
@@ -23,10 +23,10 @@ exports.seed = function(knex, Promise) {
     FROM dbo.offender_managers_archive_view om
     JOIN dbo.OrganisationalUnit ouLdu ON om.workload_ldu_id = ouLdu.Id
     LEFT JOIN dbo.Note n ON om.om_id = n.OffenderManagerId
-    GROUP BY om.UniqueIdentifier, om.om_type_id, ouLdu.Name, om.team_name, 
-    om.forename, om.surname, w.NominalTarget, w.TotalCases, w.TotalPoints, w.SDRPoints, 
-    w.SDRConversionPoints, w.PAROMSPoints, w.ContractedHoursPerWeek, 
-    w.hoursReduction, reduction, comments, reduction_date, reduction_added_by`
+    GROUP BY om.unique_identifier, om.om_type_id, ouLdu.Name, om.team_name, 
+    om.om_forename, om.om_surname, om.nominal_target, om.total_cases, om.total_points, om.sdr_points, 
+    om.sdr_conversion_points, om.paroms_points, om.contracted_hours, 
+    om.hours_reduction, n.HoursReduced, n.Notes, n.LastUpdateDateTime, n.LastUpdateUserId`
 
     return knex.schema
     .raw('DROP VIEW IF EXISTS dbo.archive_data_view;')
