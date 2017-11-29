036f08ab3322603e59eb272901fbb2e35e65e806
David McKinney <d.mckinney@kainos.com>
2017-11-29 18:03:55 +0000

    Create two indexed views for offender managers and reductions which are joined by a final archive view

--- seed/archive-views/00300_archive_data_view.js
+++ seed/archive-views/00300_archive_data_view.js
@@ -16,20 +16,20 @@ exports.seed = function(knex, Promise) {
     , om.nominal_target AS nominal_target
     , SUM(om.contracted_hours) AS contracted_hours 
     , SUM(om.hours_reduction) AS hours_reduction
-    , n.HoursReduced AS reduction
-    , n.Notes AS comments
-    , n.LastUpdateDateTime AS reduction_date
-    , n.LastUpdateUserId AS reduction_added_by
+    , n.reduction AS reduction
+    , n.comments AS comments
+    , n.reduction_date AS reduction_date
+    , n.reduction_added_by AS reduction_added_by
     FROM dbo.offender_managers_archive_view om
     JOIN dbo.OrganisationalUnit ouLdu ON om.workload_ldu_id = ouLdu.Id
-    LEFT JOIN dbo.Note n ON om.om_id = n.OffenderManagerId
+    LEFT JOIN dbo.reductions_archive_view n ON om.om_id = n.offender_manager_id
     GROUP BY om.unique_identifier, om.om_type_id, ouLdu.Name, om.team_name, 
     om.om_forename, om.om_surname, om.nominal_target, om.total_cases, om.total_points, om.sdr_points, 
     om.sdr_conversion_points, om.paroms_points, om.contracted_hours, 
-    om.hours_reduction, n.HoursReduced, n.Notes, n.LastUpdateDateTime, n.LastUpdateUserId`
+    om.hours_reduction, n.reduction, n.comments, n.reduction_date, n.reduction_added_by`
 
     return knex.schema
     .raw('DROP VIEW IF EXISTS dbo.archive_data_view;')
     .raw('SET ARITHABORT ON')
-    //.raw(view)
+    .raw(view)
 }
